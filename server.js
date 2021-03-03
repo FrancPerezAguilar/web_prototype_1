a76a57c1532641ac7d1f963fece68ee43aaf166a
Franc Pérez <franc_p@MacBook-Pro-de-Franc.local>
2021-03-03 13:14:49 +0100

    Commit 03/03/2021

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const router = express.Router();
+const port = 3000;
+
+
+app.use('/', express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
